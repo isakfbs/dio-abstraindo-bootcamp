da1f3ebcc83fb0f309eb2737b922059302a89e81
Isak Silva <isakfreires98@gmail.com>
2024-10-23 15:11:38 -0300

    Create Abstract Class

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,8 +1,16 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso = new Curso();
+        Curso cursoJava = new Curso("Curso Java","descrição do curso de Java",80);
+        Curso cursoJS = new Curso("Curso Javascript","descrição do curso de Javascript",70);
+        Mentoria mentoria = new Mentoria("Mentoria de GO","Descrição da Mentoria", LocalDate.now());
 
+        System.out.println(mentoria);
+        System.out.println(cursoJS);
+        System.out.println(cursoJava);
     }
 }
